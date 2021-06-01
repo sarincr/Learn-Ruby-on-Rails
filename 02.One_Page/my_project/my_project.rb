d24ca556c2b8958d0786af11f723784729b086a3
sarincr <sarinrays@gmail.com>
2021-06-01 07:22:03 +0530

    One Page

---
+++ 02.One_Page/my_project/my_project.rb
@@ -0,0 +1,12 @@
+  class MyProject < ArduinoSketch
+    
+    # looking for hints?  check out the examples directory
+    # example sketches can be uploaded to your arduino with
+    # rake make:upload sketch=examples/hello_world
+    # just replace hello_world with other examples
+    
+      def loop
+       # your code here
+      end
+      
+  end
